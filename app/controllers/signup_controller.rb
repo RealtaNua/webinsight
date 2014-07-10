ac564bbe784ddc89969f47db2c23633fb72b8cf4
chinsiongk <chinsiongk@gmail.com>
2014-07-10 20:30:22 +0800

    changed logo and added sharethis

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -10,7 +10,7 @@ class SignupController < ApplicationController
   	    @lead = Lead.new(:email => params['email'],:channel => params['channel'])
   	    @lead.save
   	    
-  	    flash[:notice] = 'Thanks for your support, we will keep you in the loop!'
+  	    flash[:notice] = "Thanks, we'll keep you in the loop!"
   	    redirect_to action: 'index'
   end
   

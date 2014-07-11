c96da201c0381cae16314104e0a9d3805a2ececf
chinsiongk <chinsiongk@gmail.com>
2014-07-11 18:38:01 +0800

    added check for duplicate email

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -8,10 +8,13 @@ class SignupController < ApplicationController
   
   def email
   	    @lead = Lead.new(:email => params['email'],:channel => params['channel'])
-  	    @lead.save
-  	    
-  	    flash[:notice] = "Thanks, we'll keep you in the loop!"
-  	    redirect_to '/thanks-for-signing-up'
+  	    if @lead.save
+  	    	flash[:notice] = "Thanks, we'll keep you in the loop!"
+  	    	redirect_to '/thanks-for-signing-up'
+  	    else
+  	    	flash[:error] = "Sorry, your email address is either invalid or already with us"
+  	    	redirect_to '/'
+  	    end
   end
   
 end
